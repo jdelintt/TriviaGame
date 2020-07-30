b164be053c934fbd544d23381cc3382bed82d8d0
Jesus DeLintt <jesus.delintt@gmail.com>
2020-07-30 10:53:13 -0700

    added timer

--- trivia.js
+++ trivia.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   var triviaGameAppear = document.querySelector(".trivia-game");
   var clickButton = document.querySelector(".start-button");
+  var counter = 0;
+  timerSet = document.querySelector(".timer");
+
   console.log(clickButton);
   clickButton.addEventListener("click", function () {
     alert("Hello");
     triviaGameAppear.removeAttribute("style");
     triviaGameAppear.style.display = "block";
+    timerSet.appendChild(counter);
+    setTime();
   });
+  function setTime() {
+    counter++;
+  }
+  setInterval(setTime, 1000);
 });
